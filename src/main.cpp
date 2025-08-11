d76beed0c1b1803fb0a1720c8b58b7cc3c18d8f7
mrpoulpy <mrpoulpy@example.com>
2025-08-11 21:58:04 +0200

    début travail tsunami

--- src/main.cpp
+++ src/main.cpp
@@ -45,8 +45,13 @@ typedef struct {
 #define BUTTON1 D5
 #define BUTTON2 D6
 
-#include "SparkFun_Qwiic_MP3_Trigger_Arduino_Library.h" //http://librarymanager/All#SparkFun_MP3_Trigger
-MP3TRIGGER mp3;
+//#include "SparkFun_Qwiic_MP3_Trigger_Arduino_Library.h" //http://librarymanager/All#SparkFun_MP3_Trigger
+#include <SparkFun_Tsunami_Qwiic.h> //http://librarymanager/All#SparkFun_Tsunami_Super_WAV_Trigger
+
+TsunamiQwiic tsunami;
+char versionResponse[36];
+
+//MP3TRIGGER mp3;
 int songnumber = 1;
 int nbsongs = 30;
 
@@ -70,26 +75,12 @@ void OnDataRecv(unsigned char *mac, unsigned char *incomingData, unsigned char l
         if (msg.type == MSG_TYPE_ANIMATION_TRIGGER) {
             Serial.printf("Déclenchement de l'animation %d\n", msg.animationNumber);
             
-            // Vérifier si une lecture est en cours
-            if (mp3.isPlaying()) {
-                Serial.println("Arrêt de la lecture en cours pour le retrigger...");
-                mp3.stop();
-                delay(50); // Petit délai pour s'assurer que l'arrêt est effectif
-            }
-            
-            // Forcer le volume au maximum pour éviter le fade-in
-            mp3.setVolume(31);
-            
-            // Jouer la piste
-            mp3.playTrack(msg.animationNumber);
-            
-            // Attendre un peu puis afficher le nom du fichier
-            delay(100);
-            String fileName = mp3.getSongName();
-            Serial.printf("Lecture du fichier: %s\n", fileName.c_str());
+            // Vérifier si une lecture est en cours            
+            tsunami.trackPlaySolo(msg.animationNumber, 0);
+            Serial.printf("Lecture de la track %d\n", msg.animationNumber);
         } else if (msg.type == MSG_TYPE_STOP_PLAYBACK) {
             Serial.println("Arrêt de la lecture demandé");
-            mp3.stop();
+            tsunami.trackStop(msg.animationNumber);
         } else {
             Serial.printf("Type de message non reconnu: 0x%02X\n", msg.type);
         }
@@ -128,73 +119,29 @@ void setup()
 
   Wire.begin();
   
-  //Check to see if Qwiic MP3 is present on the bus
-  if (mp3.begin() == false)
+  while(tsunami.begin() == false)
   {
-    Serial.println("Qwiic MP3 failed to respond. Please check wiring and possibly the I2C address. Freezing...");
-    while (1)
-      ;
+    Serial.println("Tsunami Qwiic failed to respond. Please check wiring and possibly the I2C address. Waiting 1 second...");
+    delay(1000);
   }
 
-  if (mp3.hasCard() == false)
-  {
-    Serial.println("Qwiic MP3 is missing its SD card. Freezing...");
-    while (1)
-      ;
-  }
 
   Serial.print("Version du firmware: ");
-  Serial.println(mp3.getVersion());
+  Serial.println(tsunami.getVersion(versionResponse));
 
   Serial.println("Reset du module");
   
-  mp3.setVolume(28); //Volume can be 0 (off) to 31 (max)
-  mp3.setEQ(0);
-  
-  // Essayer de désactiver le fade-in en utilisant d'autres paramètres
-  // Note: Le fade-in peut être une fonctionnalité matérielle du module
-  
-  // Alternative: essayer de forcer le volume immédiatement
-  mp3.setVolume(28); // Réappliquer le volume pour s'assurer qu'il n'y a pas de rampe
-
-  Serial.print("Song count: ");
-  nbsongs=mp3.getSongCount();
-  Serial.println(nbsongs);
-
-  Serial.print("Volume level: ");
-  Serial.println(mp3.getVolume());
-
-  Serial.print("EQ Setting: ");
-  byte eqSetting = mp3.getEQ();
-  if (eqSetting == 0)
-    Serial.print("Normal");
-  else if (eqSetting == 1)
-    Serial.print("Pop");
-  else if (eqSetting == 2)
-    Serial.print("Rock");
-  else if (eqSetting == 3)
-    Serial.print("Jazz");
-  else if (eqSetting == 4)
-    Serial.print("Classic");
-  else if (eqSetting == 5)
-    Serial.print("Bass");
-  Serial.println();
-
-  Serial.print("Firmware version: ");
-  Serial.println(mp3.getVersion());
-
   // Configuration ESP-NOW en mode récepteur
-  if (!initESPNow()) {
-    Serial.println("Échec de l'initialisation d'ESP-NOW");
-    while (1) ; // Arrêter si ESP-NOW ne peut pas être initialisé
+  while (!initESPNow()) {
+    Serial.println("Échec de l'initialisation d'ESP-NOW, waiting 1 second...");
+    delay(1000);
   }
 
   // Jouer le premier fichier au démarrage
   Serial.print("Playing the first track: ");
-  mp3.playFile(1);
+  tsunami.trackPlaySolo(1, 0);
   delay(2000); //Give the WT2003S a bit of time before we check to see which track is playing (allow song to start)
-  String songName = mp3.getSongName();
-  Serial.println(songName);
+  
 }
 
 void loop()
